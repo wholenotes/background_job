0678d6a403719459c7474146f609bd52f8ffd7d0
Bonnie Kwong <sibilant.threads@gmail.com>
2012-09-04 22:03:00 -0700

    last push

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,9 @@ function check_job() {
 }
 
 function callback(response) {
-    var status = response.status;
+    var status = response.status
+
+
     var elapsed = (Date.now() - BatchHelper.start_time) / 1000;
     $('#spawn').text('Job status: ' + status + '. Elapsed time: ' + elapsed + ' seconds.');
     switch(status) {
